c4adb28c120fbb2fb30d82acc86ddbc86b9d330c
Ghoufran Zouebi <78136843+Ghoufran97Z@users.noreply.github.com>
2023-09-20 01:30:27 +0300

    create empty application

---
+++ ProjectAngularFireBase/furniture-shopping-app/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { CartComponent } from './components/cart/cart.component';
+import { FurnituresComponent } from './components/furnitures/furnitures.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'admin', component: FurnituresComponent },
+  { path: '**', component: NotFoundComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
