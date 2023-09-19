a6261fa9aa25a67b4e6d16bea8e13e8cd99a8cbc
Ghoufran Zouebi <78136843+Ghoufran97Z@users.noreply.github.com>
2023-09-20 02:16:32 +0300

    finilize nav

--- ProjectAngularFireBase/furniture-shopping-app/src/app/components/nav-bar/nav-bar.component.ts
+++ ProjectAngularFireBase/furniture-shopping-app/src/app/components/nav-bar/nav-bar.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.css']
+  styleUrls: ['./nav-bar.component.css'],
 })
-export class NavBarComponent {
-
+export class NavBarComponent implements OnInit {
+  isOpen:boolean =false;
+  constructor(){}
+  ngOnInit() {}
+  toggleNavBar(){
+    this.isOpen=!this.isOpen
+  }
 }
